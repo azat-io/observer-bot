56927255e66aac77893512a4cd53098b5320427a
fletcherist <romanovexports@gmail.com>
2017-08-12 19:55:30 +0300

    Add validation

--- components/signup.js
+++ components/signup.js
@@ -1,17 +1,25 @@
 import { saveUser } from './api/users'
 
+
+const validateUsername = username => username.match(/^[a-zа-я ]{1,80}$/ig)
+const validateCity = city => city.match(/^[a-zа-я]{1,60}$/ig)
+const validateTwitter = twitter => twitter.match(/^[a-z]{1,60}$/ig)
+
 const STEPS = {
     1: {
         question: 'Как тебя зовут?',
         dataModel: 'firstName',
+        validateFunction: validateUsername
     },
     2: {
         question: 'Из какого ты города?',
         dataModel: 'city',
+        validateFunction: validateCity
     },
     3: {
         question: 'Введи свой юзернейм в Твиттере',
         dataModel: 'twiiter',
+        validateFunction: validateTwitter
     },
 }
 
@@ -19,6 +27,7 @@ const dataStorage = {}
 
 const getCurrentStep = telegramId => dataStorage[telegramId].step
 const getCurrentQuestion = telegramId => STEPS[getCurrentStep(telegramId)].question
+const getValidateFunction = telegramId => STEPS[getCurrentStep(telegramId)].validateFunction
 
 function updateData (telegramId, data) {
     const newUserData = Object.assign(dataStorage[telegramId].data, {
@@ -60,19 +69,31 @@ function sendMessageToBot (_telegramId) {
     }
 }
 
-// const validateData = _telegramId => {
-//     const telegramId = _telegramId
-//     return (data) => {
-
-//     }
-// }
+/**
+ * Use this to get to know,
+ * if data sent by user is valid
+ * @param _telegram {String}
+ * @return true/false {Boolean}
+*/
+const dataValidator = _telegramId => {
+    const telegramId = _telegramId
+    return data => getValidateFunction(telegramId)(data)
+}
 
 export default function signup (telegramId, message) {
     const sendMessage = sendMessageToBot(telegramId)
-    if (message.trim() === 'Зарегистрироваться') {
+    const isValidData = dataValidator(telegramId)
+    message = message.trim()
+
+    if (message === 'Зарегистрироваться') {
         initializeSignup(telegramId)
-        return sendMessage(STEPS[dataStorage[telegramId].step].question)
+        return sendMessage(getCurrentQuestion(telegramId))
     }
+
+    if (!isValidData(message)) {
+        return sendMessage('Кажется, введены неправильные данные. Попробуй ещё раз')
+    }
+
     updateData(telegramId, message.trim())
 
     if (getCurrentStep(telegramId) === 4) {
