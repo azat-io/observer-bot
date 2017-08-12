9220066687db0cf9239a84fa70118bb260334c87
Azat S <to@azat.io>
2017-08-12 15:45:09 +0300

    Add first button

--- components/app.js
+++ components/app.js
@@ -4,5 +4,14 @@ import bot from './telegram-bot'
 import readMD from './read-markdown'
 
 bot.onText(/^(\/start)$/, msg => {
-    bot.sendMessage(msg.chat.id, readMD('start'))
+    bot.sendMessage(msg.chat.id, readMD('start'), {
+        parse_mode: 'markdown',
+        disable_web_page_preview: true,
+        reply_markup: JSON.stringify({
+            keyboard: [
+                ['Зарегистрироваться'],
+            ],
+            resize_keyboard: true,
+        }),
+    })
 })
